f46ec384f7729957d3b8bdf2f1b6399a9ffaf8dd
Syed-Sabeer <syedsabeer.techie@gmail.com>
2025-01-19 20:44:27 +0500

    user migration updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status');
             $table->boolean('is_reseller')->default(0);
             $table->string('neq_number')->nullable();
             $table->rememberToken();
