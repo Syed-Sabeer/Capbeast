6f998dfe2791e973004687373a4553ac8c398b81
Syed-Sabeer <syedsabeer.techie@gmail.com>
2025-01-29 23:41:10 +0500

    public access removed

--- config/app.php
+++ config/app.php
@@ -56,8 +56,8 @@ return [
     */
 
   'url' => env('APP_URL', 'http://localhost'),
-
-  'asset_url' => env('ASSET_URL'),
+'asset_url' => env('ASSET_URL', 'http://localhost/Beanies/public'),
+  // 'asset_url' => env('ASSET_URL'),
 
   /*
     |--------------------------------------------------------------------------
