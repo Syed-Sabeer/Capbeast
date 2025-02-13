5df85e7fd3939bc6be41279dc436efa261b432e3
Syed-Sabeer <syedsabeer.techie@gmail.com>
2025-02-13 19:08:45 +0500

    template updated

--- resources/views/emails/user_registered.blade.php
+++ resources/views/emails/user_registered.blade.php
@@ -5,12 +5,11 @@
 </head>
 <body>
 
-    @php
-        $lang = $user->language ?? 'en'; 
-    @endphp
 
 
-@if($lang == 'fr')
+
+@if($user->language == 'fr')
+
     <h2>Cher, {{ $user->first_name ?? 'Client précieux' }}!</h2>
     <h4>Merci de vous inscrire avec Monkey Beanies! Nous sommes ravis de vous avoir à bord. </h4>
     <p> Si vous avez des questions ou des préoccupations, veuillez consulter notre section FAQ ou contacter notre équipe d'assistance pour obtenir de l'aide.</p>
@@ -21,6 +20,7 @@
 @else
 
     <h2>Dear, {{ $user->first_name ?? 'Valuable Customer' }}!</h2>
+    <h2>lang, {{ $user->language ?? 'Valuable Customer' }}!</h2>
     <h4>Thank you for registering with Monkey Beanies! We're excited to have you on board. </h4>
     <p>If you have any questions or concerns, please refer to our FAQ section or contact our support team for assistance.</p>
     <p>Thank you for choosing Monkey Beanies. We look forward to serving you and providing you with an outstanding experience.Warm regards,</p>
