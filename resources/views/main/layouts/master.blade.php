786acd171f21ad8dbb5ce595ed382756031f2b25
Syed-Sabeer <syedsabeer.techie@gmail.com>
2025-03-24 13:45:38 +0500

    working

--- resources/views/main/layouts/master.blade.php
+++ resources/views/main/layouts/master.blade.php
@@ -25,36 +25,16 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Google Analytics -->
-{{-- <script async src="https://www.googletagmanager.com/gtag/js?id=X0P2QVG8V9"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'X0P2QVG8V9');
-</script> --}}
 
 
 
-<head>
-    <!-- Other head content -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X0P2QVG8V9"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-X0P2QVG8V9');
-    </script>
-</head>
-
-<!-- End Google Analytics -->
 
     
 </head>
 
 <body>
 
-    <div id="google_translate_element"></div>
+
 
 
 
@@ -70,7 +50,7 @@
     <button onclick="topFunction()" class="btn btn-info btn-icon" style="bottom: 50px;" id="back-to-top">
         <i class="ri-arrow-up-line"></i>
     </button>
-    <!--end back-to-top-->
+    
 
 
 
@@ -80,50 +60,24 @@
 
 
 
+
     <!-- JAVASCRIPT -->
-    <script src="{{ asset('assetsMain/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{ asset('assets/js/plugins.js')}}"></script>
+
+    <!-- isotope-layout -->
+    <script src="{{ asset('assets/libs/isotope-layout/isotope.pkgd.min.js')}}"></script>
+
+    <!--Swiper slider js-->
+    <script src="{{ asset('assets/libs/swiper/swiper-bundle.min.js')}}"></script>
     
+    <!-- Countdown js -->
+    <script src="{{ asset('assets/js/pages/coming-soon.init.js')}}"></script>
+    
+    <script src="{{ asset('assets/js/frontend/landing-index.init.js')}}"></script>
 
-    <script type="text/javascript">
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement({
-                pageLanguage: 'en',
-                includedLanguages: 'en,fr,pt-PT,es',
-                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-            }, 'google_translate_element');
-        }
-    </script>
-
-
-    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
-
-    <!-- Meta Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '527798759752509');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=527798759752509&ev=PageView&noscript=1" /></noscript>
-    <!-- End Meta Pixel Code -->
+    <script src="{{ asset('assets/js/frontend/menu.init.js')}}"></script>
 
 </body>
 
