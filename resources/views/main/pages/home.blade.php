73441d11652aa3633d78448c44993be692359b9b
Syed-Sabeer <syedsabeer.techie@gmail.com>
2025-03-25 13:29:41 +0500

    working on front end for products pages

--- resources/views/main/pages/home.blade.php
+++ resources/views/main/pages/home.blade.php
@@ -2,26 +2,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="{{ asset('assetsMainMain/js/frontend/owl.carousel.min.js') }}"></script>
 <link href="{{ asset('assetsMainMain/css/owl.carousel.min.css') }}" rel="stylesheet" type="text/css" />
-<<<<<<< HEAD
 
 
 
-=======
-
-<title>Customize Embroidered Beanies in Montreal-Nord, Canada</title>
-<meta name="description"
-    content="Customize Embroidered Beanies in Montreal-Nord, Canada, with unique designs. Get high-quality, stylish, and cozy Customize Embroidered Beanies for any occasion.">
-<meta name="keywords"
-    content="customize beanies, embroidered beanies, Customize Embroidered Beanies in Montreal-Nord, Customize Embroidered Beanies in Canada">
-<link rel="canonical" href="https://monkeybeanies.com/main/home">
->>>>>>> e5cafc9 (many frontend changes)
 
 
 @section('main-container')
 
 <section class="position-relative">
   <div id="ecommerceHero" class="carousel slide" data-bs-ride="carousel">
-<<<<<<< HEAD
       <div class="carousel-inner">
           <div class="carousel-item active">
               <div class="ecommerce-home bg-danger-subtle" style="
@@ -89,134 +78,6 @@
 
 
 
-=======
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <div
-          class="ecommerce-home bg-danger-subtle"
-          style="
-            background-image: url('assetsMain/images/ecommerce/home/img-1.png');
-          "
-        >
-          <div class="container">
-            <div class="row justify-content-end">
-              <div class="col-lg-7">
-                <div class="text-sm-end">
-                  <p class="fs-15 fw-semibold text-uppercase">
-                    <i
-                      class="ri-flashlight-fill text-primary align-bottom me-1"
-                    ></i>
-                    In this season, find the best
-                  </p>
-                  <h1
-                    class="display-4 fw-bold lh-base text-capitalize mb-3"
-                  >
-                    Exclusive collection for everyone
-                  </h1>
-                  <p class="fs-20 mb-4">Biggest offers on this season</p>
-                  <button class="btn btn-danger btn-hover">
-                    <i class="ph-shopping-cart align-middle me-1"></i> Shop
-                    Now
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div
-          class="ecommerce-home bg-primary-subtle"
-          style="
-            background-image: url('assetsMain/images/ecommerce/home/img-2.png');
-          "
-        >
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-6">
-                <div>
-                  <p class="fs-15 fw-semibold text-uppercase">
-                    <i
-                      class="ri-flashlight-fill text-info align-bottom me-1"
-                    ></i>
-                    Save up to <span class="text-danger">50%</span> off
-                  </p>
-                  <h1 class="display-4 fw-semibold text-capitalize lh-base">
-                    Pro Smart watches for smart People
-                  </h1>
-                  <p class="fs-18 mb-4">
-                    <b>40% off</b> up to on all selected products
-                  </p>
-                  <button class="btn btn-primary btn-hover">
-                    <i class="ph-shopping-cart align-middle me-1"></i> Shop
-                    Now
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div
-          class="ecommerce-home"
-          style="
-            background-image: url('assetsMain/images/ecommerce/home/img-6.jpg');
-            background-size: cover;
-          "
-        >
-          <div class="container">
-            <div class="row justify-content-end">
-              <div class="col-lg-6">
-                <div class="text-end">
-                  <p class="fs-15 fw-semibold text-uppercase text-dark">
-                    Jewelry Design with Love
-                  </p>
-                  <h1
-                    class="display-4 fw-semibold text-capitalize lh-base text-dark"
-                  >
-                    Discover world best Jewelry
-                  </h1>
-                  <p class="text-dark lead fs-19 mb-4">
-                    Jewelry are like a tribute to being a woman
-                  </p>
-                  <div class="hstack gap-2 justify-content-end">
-                    <button class="btn btn-success btn-hover">
-                      Shop Now
-                      <i class="ph-arrow-up-right align-middle ms-1"></i>
-                    </button>
-                    <button class="btn btn-ghost-secondary btn-hover">
-                      Watch Now
-                      <i class="ph-play-circle align-middle ms-1 fs-16"></i>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <button
-      class="carousel-control-prev"
-      type="button"
-      data-bs-target="#ecommerceHero"
-      data-bs-slide="prev"
-    >
-      <i class="ph-caret-left"></i>
-    </button>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#ecommerceHero"
-      data-bs-slide="next"
-    >
-      <i class="ph-caret-right"></i>
-    </button>
-  </div>
-</section>
-
->>>>>>> e5cafc9 (many frontend changes)
 <section class="section">
   <div class="container">
     <div class="row gy-4 gy-lg-0">
